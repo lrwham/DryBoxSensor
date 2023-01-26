d6eb564ba4b210a8b00974ffd032c5e1ff16a615
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-01-25 20:57:22 -0500

    Create template.secretConfig.h

---
+++ src/template.secretConfig.h
@@ -0,0 +1,12 @@
+#ifndef secretConfig_h
+#define secretConfig_h
+
+// or if you need your Adafruit IO key.
+#define IO_USERNAME ""
+#define IO_KEY ""
+
+/**************************** WiFi Config ***********************************/
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+#endif

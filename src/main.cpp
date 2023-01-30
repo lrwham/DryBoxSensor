395861425a1cdf2e7b4b106b1ea5f055360e8087
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-01-29 21:50:27 -0500

    Tested values for battery level ADC readings

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ AdafruitIO_Feed *battery = io.feed("drybox.battery");
 
 Adafruit_Si7021 sensor = Adafruit_Si7021();
 
-void handleMessage(AdafruitIO_Data *data)
+void handleHeaterMessage(AdafruitIO_Data *data)
 {
 
   if (data->toPinLevel() == HIGH)
@@ -36,15 +36,8 @@ void handleMessage(AdafruitIO_Data *data)
   Serial.println(sensor.isHeaterEnabled() ? "Heater is on." : "Heater is off.");
 }
 
-int battery_level()
+int battery_level_percent()
 {
-
-  // read the battery level from the ESP8266 analog in pin.
-  // analog read level is 10 bit 0-1023 (0V-1V).
-  // our 1M & 220K voltage divider takes the max
-  // lipo value of 4.2V and drops it to 0.758V max.
-  // this means our min analog read value should be 580 (3.14V)
-  // and the max analog read value should be 774 (4.2V).
 
   int level = 0;
 
@@ -60,10 +53,6 @@ int battery_level()
 
   // convert battery level to percent
   level = map(level, MIN_ANALOG_READING, MAX_ANALOG_READING, MIN_BATTERY_CAPACITY, MAX_BATTERY_CAPACITY);
-
-  Serial.print("Battery level: ");
-  Serial.print(level);
-  Serial.println("%");
 
   return level;
 }
@@ -96,7 +85,7 @@ void setup()
   Serial.println();
   Serial.println(io.statusText());
 
-  digital->onMessage(handleMessage);
+  digital->onMessage(handleHeaterMessage);
   digital->get();
 
   sensor.begin();
@@ -107,31 +96,31 @@ void setup()
   sensor.setHeatLevel(SI_HEATLEVEL_LOW);
 }
 
+/***************
+ * as a practical matter, there is not really a loop()
+ * method. After deep sleep, the sketch starts back at the top.
+ */
 void loop()
 {
 
-  // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
-  io.run();
-  int level = battery_level();
+  int level = battery_level_percent();
+
   Serial.println(battery->save(level) ? "Success! Battery feed updated." : "Failed to update Battery Feed");
 
-  // io.run();
   float celsius = sensor.readTemperature();
   Serial.println(temperature->save(celsius) ? "Success! Temperature feed updated." : "Failed to update Temperature Feed");
 
-  // io.run();
   float sensorHumidity = sensor.readHumidity();
   Serial.println(humidity->save(sensorHumidity) ? "Success! Humidity feed updated." : "Failed to update Humidity Feed");
 
-  // io.run();
   digitalWrite(GPIO_POWER_SI7021, LOW);
 
   io.run();
 
   // serial console
+  Serial.print("Battery level: ");
+  Serial.print(level);
+  Serial.println("%");
   Serial.print("celsius: ");
   Serial.print(celsius);
   Serial.println("C");
@@ -141,6 +130,9 @@ void loop()
 
   // Deepsleep to save battery life
   Serial.println("zzzz");
+  Serial.print("Sleeping for ");
+  Serial.print(DEEPSLEEP_SECONDS);
+  Serial.println("seconds.");
   ESP.deepSleep(DEEPSLEEP_INTERVAL);
 }
 
--- src/template.secretConfig.h
+++ src/template.secretConfig.h
@@ -1,7 +1,7 @@
 #ifndef secretConfig_h
 #define secretConfig_h
 
-// or if you need your Adafruit IO key.
+// set Adafruit IO key.
 #define IO_USERNAME ""
 #define IO_KEY ""
 
